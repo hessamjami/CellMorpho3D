3b31e020bb85a5e717c42fe4c0fe27ab234bebb3
Hessam Jami <125889429+hessamjami@users.noreply.github.com>
2023-10-25 16:35:56 +1100

    Add files via upload

---
+++ General_ECM.py
@@ -0,0 +1,7 @@
+ECM_num_of_Nodes = 4079
+ECM_Surface_num_of_Triangles = 1052
+ECM_Thickness = 10.0
+Membrane_Centre_distance_from_ECM = 11.30
+ECM_x_scaling_factor = 1.7
+ECM_y_scaling_factor = 1.7
+ECM_z_scaling_factor = 1.7
